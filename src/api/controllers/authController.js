2bf389c05760d2732c47002ab4fe472b5042810b
gbrltorres <gabrieltbahia@gmail.com>
2023-11-16 14:01:14 -0300

    fix: remove console log

--- src/api/controllers/authController.js
+++ src/api/controllers/authController.js
@@ -10,15 +10,11 @@ export const login = async (req, res) => {
 
         const user = await userRepository.findByEmail(email);
 
-        console.log('teste', user);
-
         if (!user) {
             return res.status(404).json({ message: 'Nenhum cadastro foi encontrado com o e-mail fornecido' })
         }
 
-        console.log('session 1', req.session);
         req.session.email = { userEmail: email };
-        console.log('session 2', req.session);
         res.send('Login bem-sucedido.');
     } catch (ex) {
         return res.status(502).json({ message: 'Ocorreu algum erro.'})
