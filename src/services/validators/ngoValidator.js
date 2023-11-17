00ede5f684d9f8f2edc2bb1e28d0a33c0ada0387
gbrltorres <gabrieltbahia@gmail.com>
2023-11-17 15:04:10 -0300

    feat: add phone number validation

--- src/services/validators/ngoValidator.js
+++ src/services/validators/ngoValidator.js
@@ -1,9 +1,11 @@
 import Joi from 'joi';
 
+const phoneNumberRegex = /^[1-9]{1}[1-9]{1}9[1-9][0-9]{7}$/;
+
 const ngoValidationSchema = Joi.object({
     name: Joi.string().required(),
     code: Joi.string().required(),
-    phoneNumber: Joi.string().required(),
+    phoneNumber: Joi.string().pattern(phoneNumberRegex).required(),
     interests: Joi.array().required()
 });
 
